f5e75e28bce4f7e173c72c166903578597c3ea40
Christopher Yovanovitch <yovano_c@outlook.com>
2017-12-14 15:56:44 +0100

    cleaning

--- dist/AccountsGenerator.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class AccountsGenerator {
-    static generateWithProxy(proxyPath: string, total: number, output: string): Promise<void>;
-    static generateWithoutProxy(total: number, output: string): Promise<void>;
-    private static readonly mailsac;
-    private static generate(output, proxyPath?);
-}
