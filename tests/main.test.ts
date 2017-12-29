63df84e420c8100579e881e746c3821e7792807d
Christopher Yovanovitch <yovano_c@outlook.com>
2017-12-29 16:18:33 +0100

    fixed test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,34 +1,33 @@
 import * as chai from "chai";
 import * as mocha from "mocha";
-
-import Ankarton from '../dist/index'
+import Ankarton from "../dist/index";
 
 const expect = chai.expect;
 
 describe("AccountsGenerator", () => {
 
-	it.skip("should create one account", (done) => {
-		Ankarton.generate({}).then(() => { done() })
-	});
-	it.skip("should create one account", (done) => {
-		Ankarton.generate({
-			output: (err: any, data: any) => {
-				done()
-			}
-		}).then(() => { })
-	});
+  it.skip("should create one account", (done) => {
+    Ankarton.generate({}).then(() => { done(); });
+  });
+  it.skip("should create one account", (done) => {
+    Ankarton.generate({
+      output: (err: any, data: any) => {
+        done();
+      },
+    }).then(() => {/**/});
+  });
 
-	it("should create one account", (done) => {
-		Ankarton.generate({
-			proxy: { host: "", port: 4201 },
-			output: (err: any, data: any) => {
-				console.log(data)
-				done()
-			},
-			passwordGenerator: (guestLogin: string, guestPassword: string): string => {
-				return "genpas246"
-			},
-		}).then(() => { })
-	});
+  it("should create one account", (done) => {
+    Ankarton.generate({
+      output: (err: any, data: any) => {
+        console.log(data);
+        done();
+      },
+      passwordGenerator: (guestLogin: string, guestPassword: string): string => {
+        return "genpas246";
+      },
+      proxy: { host: "", port: 4201 },
+    }).then(() => {/**/});
+  });
 
 });
